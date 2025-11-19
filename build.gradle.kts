441a664dd841067e190594e971cddd95d652583f
rgw65206-sketch <rgw65206@gmail.com>
2025-11-19 15:17:38 +0900

    2-02 간단한 웹 프로그램 만들기

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.mysite"
 version = "0.0.1-SNAPSHOT"
-description = "sbb"
 
 java {
     toolchain {
