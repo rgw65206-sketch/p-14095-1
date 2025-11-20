ee62106bacae9d56099f028407eaa07f91608056
rgw65206-sketch <rgw65206@gmail.com>
2025-11-20 14:28:48 +0900

    19강

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,7 +1,15 @@
 package com.mysite.sbb;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Optional<Question> findBySubject(String subject);
+
+    Optional<Question> findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subjectLike);
 }
